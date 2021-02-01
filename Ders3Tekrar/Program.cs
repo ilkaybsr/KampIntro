c7d18c6babcbdd05103c77372241cab1a500775d
ilkaybsraydin@hotmail.com <ilkaybsraydin@hotmail.com>
2021-02-01 14:58:36 +0300

    ref out bakıldı

---
+++ Ders3Tekrar/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+
+namespace Ders3Tekrar
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Add();
+            //var result = Add2(20,30);
+            //int number1 =20;
+            //int number2 = 100;
+            //var result2 = Add3(out number1, number2);
+            //Console.WriteLine(result2);
+            //Console.WriteLine(number1);
+
+            Console.WriteLine(Multiply(2,4,5));
+            Console.ReadLine();
+        }
+
+        static void Add() 
+        {
+            Console.WriteLine("Added!");
+        }
+
+        static int Add2(int number1, int number2 ) 
+        {
+           var result = number1 + number2;
+            return result;
+        }
+        static int Add3(out int number1, int number2) 
+        {
+            number1 = 30; 
+            return number1 + number2;
+        }
+
+        static int Multiply(int number1, int number2) 
+        {
+            return number1 * number2;
+        }
+
+        static int Multiply(int number1, int number2,int number3) 
+        {
+            return number1 * number2 * number3;
+        }
+    }
+}
---
+++ SQLQuery2.sql
@@ -0,0 +1,15 @@
+﻿--Select
+Select ContactName Adi, CompanyName SirketAdi,City Sehir from Customers
+
+Select * from Customers where City =  'Berlin'
+
+
+select * from Products where CategoryID = 1 and UnitPrice>10
+
+select * from Products where CategoryID=1 order by UnitPrice desc
+
+select count(*) adet from Products where CategoryID =2
+
+
+select  CategoryID  from Products group by CategoryID
+ 
