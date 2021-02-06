0eb15ff3e9178dba6e34ac4a95f4633726314eaf
ilkaybsraydin@hotmail.com <ilkaybsraydin@hotmail.com>
2021-02-06 21:50:42 +0300

    Generics ve Constructor Yapi

---
+++ Generics/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Generics
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<string> sehirler = new List<string>();
+            sehirler.Add("Ankara");
+            Console.WriteLine(sehirler.Count);
+            
+            sehirler.Add("Ankara");
+            Mylist<string> sehirler2 = new Mylist<string>();
+            sehirler2.Add("Ankara");
+            sehirler2.Add("Ankara");
+            Console.WriteLine(sehirler2.Count);
+        }
+    }
+
+    class Mylist<T>
+    {
+        T[] _array;
+        T[] _tempArray;
+        public Mylist()
+        {
+            _array = new T[0];
+        }
+        public void Add(T item) 
+        {
+            _tempArray = _array;
+            _array = new T[_array.Length + 1];
+            for (int i = 0; i < _tempArray.Length; i++)
+            {
+                _array[i] = _tempArray[i];
+                _array[_array.Length - 1] = item;
+            }
+        }
+
+        private int _count;
+
+        public int Count
+        {
+            get { return _array.Length; }
+        }
+    }
+}
