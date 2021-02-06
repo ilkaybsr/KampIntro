5d01e2ff7ef6672a477b6d1ff1f56740bc9c5a81
ilkaybsraydin@hotmail.com <ilkaybsraydin@hotmail.com>
2021-02-06 23:26:19 +0300

    MyDictionary Olusturuldu

---
+++ MyDictionary/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace MyDictionary
+{
+     public class Program
+    {
+        static void Main(string[] args)
+        {
+            MyDictionary<int, string> patients = new MyDictionary<int, string>();
+
+            patients.Add(20, "İlkay");
+            patients.Add(15, "Batu");
+            patients.Add(23, "Onur");
+            patients.Add(56, "Tuğçe");
+        }
+    }
+}
