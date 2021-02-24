52363cf645b62bdf76aa489d9d2862b7a0315897
ilkaybsraydin@hotmail.com <ilkaybsraydin@hotmail.com>
2021-02-24 21:19:40 +0300

    GameProject eklendi

---
+++ GameProject/CampaignManager.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace GameProject
+{
+    class CampaignManager : ICampaignService
+    {
+        public void Add(Campaign campaign)
+        {
+            Console.WriteLine(campaign.CampaignName + " kampanyası eklendi");
+        }
+
+        public void Delete(Campaign campaign)
+        {
+            Console.WriteLine(campaign.CampaignName + " kampanyası silindi");
+        }
+
+        public void Update(Campaign campaign)
+        {
+            Console.WriteLine(campaign.CampaignName + " kampanyası güncellendi");
+        }
+    }
+}
---
+++ GameProject/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace GameProject
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            GamerManager gamerManager = new GamerManager(new UserValidationManager());
+            gamerManager.Add(new Gamer { Id = 1, BirthYear = 1995, FirstName = "BÜŞRA", LastName = "AYDIN", IdentityNumber = 12345 });
+
+        }
+    }
+}
